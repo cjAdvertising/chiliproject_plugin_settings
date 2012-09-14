a3ee98928eab235a51b9e5908da9be9378624a56
Greg Thornton <xdissent@me.com>
2012-09-14 08:39:48 -0500

    Always return empty hashie

--- lib/chiliproject_plugin_settings/plugin_settings_hash.rb
+++ lib/chiliproject_plugin_settings/plugin_settings_hash.rb
@@ -8,7 +8,7 @@ module ChiliprojectPluginSettings
     end
 
     def [](module_name)
-      options = ChiliprojectPluginSettings.settings_options module_name, @object_name
+      return {} unless options = ChiliprojectPluginSettings.settings_options(module_name, @object_name)
       model = options[:model].is_a?(Symbol) ? options[:model].to_s.camelize.constantize : options[:model]
       model.send "find_by_#{@object_name}_id".to_sym, @object.id
     end
